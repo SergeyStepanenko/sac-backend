fa13e70f748a4a19361713fe85fd021ff1d0748e
Sergey Stepanenko <s.stepanenko@corp.mail.ru>
2020-07-31 23:45:23 +0300

    add post method /items

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,8 @@ import cors from 'cors'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-import { getItems } from './db/item'
+
+import { getItems, addItem, ISacItem } from './db/item'
 
 dotenv.config()
 
@@ -13,10 +14,27 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/', async (req, res) => {
+app.get('/items', async (req, res) => {
   const items = await getItems()
 
   res.send(items)
+})
+
+app.post('/items', async (req, res) => {
+  const { title, description } = req.body as ISacItem
+
+  if (typeof title !== 'string' || typeof description !== 'string') {
+    res.status(422).send({ error: 'Ошибка валидации' })
+
+    return
+  }
+
+  const item = await addItem({
+    title,
+    description
+  })
+
+  res.send(item)
 })
 
 async function start() {
